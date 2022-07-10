a0b8a55eef020feb2de6ce0585e4ada316b1280f
Sathish <sbabu@progress.com>
2022-07-10 23:21:10 +0530

    remove stuff that doesn't belong here
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/unit/libraries/k8s_config_map_test.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../../test_helper'
-require 'k8s_config_map'
-
-class K8sConfigMapConstructorTest < Minitest::Test
-  def test_empty_param_not_ok
-    assert_raises(ArgumentError) { Inspec::Resources::K8sConfigMap.new }
-  end
-end
--- test/unit/libraries/k8s_config_maps_test.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../../test_helper'
-require 'k8s_config_maps'
-
-class K8sConfigMapsConstructorTest < Minitest::Test
-  def test_empty_param_ok
-    assert(Inspec::Resources::K8sConfigMaps.new.exists?)
-  end
-end
-
