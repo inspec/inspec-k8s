17737abfc78aa2a24d2eaa5a7a0391e4383c6680
Sathish <sbabu@progress.com>
2022-07-12 18:08:46 +0530

    new way of adding the tests
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/unit/libraries/k8sobject_test.rb
+++ test/unit/libraries/k8sobject_test.rb
@@ -1,9 +1,32 @@
 require_relative '../../test_helper'
 require 'k8sobject'
 
-class K8sObjectConstructorTest < Minitest::Test
-  def test_empty_param_not_ok
-    assert_raises(ArgumentError) { Inspec::Resources::K8sObject.new }
+class K8sObjectTest < Minitest::Test
+  stub_data = {
+    'v1': {
+      default: {
+        pods: [
+          {
+            name: 'pod1',
+            kind: 'pod',
+            status: {
+              phase: 'running'
+            },
+            metadata: {
+              uid: 'abcd1234',
+              name: 'pod1',
+              namespace: 'default',
+              resourceVersion: 1234,
+              annotations: [],
+              labels: []
+            }
+          }
+        ]
+      }
+    }
+  }
+  @k8s_object = Inspec::Resources::K8sObject.new(backend: Mock::K8s::Transport.new(stub_data: stub_data))
+  def test_uid
+    assert_equal('abcd1234', @k8s_object.uid)
   end
 end
-
