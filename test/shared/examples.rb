542005f52f011ff4fe0003b93df6a4f868c6bae2
Sathish <sbabu@progress.com>
2022-07-14 20:00:53 +0530

    go for plural support
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/shared/examples.rb
+++ test/shared/examples.rb
@@ -1,3 +1,6 @@
+require 'k8sobject'
+require 'k8sobjects'
+
 module Shared
   module Examples
     NAMESPACE = 'default'.freeze
@@ -6,6 +9,14 @@ module Shared
       @k8s_object ||= Inspec::Resources::K8sObject.new(
         backend: Mock::K8s::Transport.new(stub_data: self.class::STUB_DATA),
         name: self.class::NAME,
+        type: self.class::TYPE,
+        namespace: self.class::NAMESPACE
+      )
+    end
+
+    def k8s_objects
+      @k8s_objects ||= Inspec::Resources::K8sObjects.new(
+        backend: Mock::K8s::Transport.new(stub_data: self.class::STUB_DATA),
         type: self.class::TYPE,
         namespace: self.class::NAMESPACE
       )
