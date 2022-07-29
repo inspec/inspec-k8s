73ab176e73c16f9e714a6e6ed128a5983cd6a08f
Sonu Saha <sonu.saha@progress.com>
2022-07-29 13:02:47 +0530

    TEST: Add unit test for k8s_cronjob(s) (CFINSPEC-318)
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

---
+++ test/unit/libraries/k8s_cronjob_test.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require_relative 'resource_test'
+
+class K8sCronjobTest < ResourceTest
+  STUB_DATA = {
+    'v1': {
+      default: {
+        cronjobs: [
+          {
+            name: 'hello',
+            kind: 'Cronjob',
+            metadata: {
+              uid: '378c1a39-cddc-4df6-bf5a-593779eb26fc',
+              name: 'hello',
+              namespace: 'default',
+              resourceVersion: '1234',
+              creationTimestamp: '2022-07-21T10:47:49Z',
+              annotations: { 'test_annotation1': 'abc' },
+              labels: {}
+            }
+          }
+        ]
+      }
+    }
+  }.freeze
+
+  NAME = 'hello'
+  TYPE = 'cronjobs'
+
+  def test_uid
+    assert_equal('378c1a39-cddc-4df6-bf5a-593779eb26fc', k8s_object.uid)
+  end
+
+  def test_name
+    assert_equal('hello', k8s_object.name)
+  end
+
+  def test_namespace
+    assert_equal('default', k8s_object.namespace)
+  end
+
+  def test_kind
+    assert_equal('Cronjob', k8s_object.kind)
+  end
+
+  def test_resource_version
+    assert_equal('1234', k8s_object.resource_version)
+  end
+
+  def test_labels
+    assert_equal(k8s_object.labels, {})
+  end
+
+  def test_annotations
+    assert_equal(k8s_object.annotations, { 'test_annotation1': 'abc' })
+  end
+
+  def test_creation_timestamp
+    assert_equal('2022-07-21T10:47:49Z', k8s_object.creationTimestamp)
+  end
+end
